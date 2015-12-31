eda907f95a3e155cf67918c787c6a7ad584e90ce
Kyle Annen <kyleannen@gmail.com>
2015-12-30 18:02:54 -0800

    finish trip creation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,13 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
+
+  #paperclip entries
+  attr_accessor :image
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  #validations
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :image, presence: true
 end
