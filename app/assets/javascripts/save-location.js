78839c8d213088e8c0b1ba0b11bf5726199464dd
Kyle Annen <kyleannen@gmail.com>
2016-05-26 15:24:24 -0700

    fix trip show page css and layout

--- app/assets/javascripts/save-location.js
+++ app/assets/javascripts/save-location.js
@@ -1,5 +1,5 @@
 // javascript function to access the location data as returned from the 
-$(document).ready(function($) {
+$(document).ready(function() {
   //Copy location, lat and long to rails form 
 
   //when search field is clicked it is cleared
@@ -11,12 +11,12 @@ $(document).ready(function($) {
       //Expand or collapse this panel
       $(this).next().slideToggle('slow');
       //Hide the other panels
-      $(".accordion-content").not($(this).next()).slideUp('slow');
+      $(".accordion-content").not($(this).next()).slideUp('fast');
       //clear fields on open
       $('#gmaps-input-address').val(""); 
       $('#gmaps-output-latitude').val(""); 
       $('#gmaps-output-longitude').val(""); 
-      google.maps.event.trigger(gmaps-canvas, 'resize');
+      $(window).trigger('resize');
     });
 
 
