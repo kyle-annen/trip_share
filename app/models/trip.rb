f1e85a18206626906b42dd03215b443734d7f02c
Kyle Annen <kyleannen@gmail.com>
2016-05-28 11:27:52 -0700

    change default url in trip controller has_attached_image

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,7 +4,7 @@ class Trip < ActiveRecord::Base
 
   #paperclip entries
   attr_accessor :image
-  has_attached_file :image, styles: {large: "600x600", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: {large: "600x600", medium: "300x300>", thumb: "100x100>" }, default_url: "images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   crop_attached_file :image, :aspect => "3:2"
 
