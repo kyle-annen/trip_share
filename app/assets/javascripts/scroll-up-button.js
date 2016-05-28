edf3c18d0b7157c117976d1490d0c79dd781e63d
Kyle Annen <kyleannen@gmail.com>
2016-05-28 09:54:55 -0700

    implement scroll up button

---
+++ app/assets/javascripts/scroll-up-button.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	//initially hide scroll-up-switch
+	$("#scroll-up-switch").fadeOut(0);
+
+	//when scroll down 50px, fade in scroll button
+	$(document).scroll(function() {
+	  var y = $(this).scrollTop();
+	  if (y > 50) {
+	    $('#scroll-up-switch').fadeIn();
+	  } else {
+	    $('#scroll-up-switch').fadeOut();
+	  }
+	});
+
+
+	//when scroll button clicked, scroll to top
+	$(document).on("click", "#scroll-up-switch", function(){
+		$('html, body').animate({scrollTop: 0}, 'slow');
+	})
+})
