c7398eb4c36d1f6075e9f45308c701c38f653088
Kyle Annen <kyleannen@gmail.com>
2016-06-16 19:38:29 -0700

    add browser geolocation to trip page

--- app/assets/javascripts/google-maps.js
+++ app/assets/javascripts/google-maps.js
@@ -1,24 +1,60 @@
 var map;
+var lat = -33.8665433;
+var long = 151.1956316;
 
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("gmaps-canvas"), mapProp);
+//get coordinates
+function getLocation() {
+  //if geolocation is available, set to location, otherwise default to seattle
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setLatLong);
+  }
+}
+
+//set the latititude and longitude variable to the gmaps center tag format
+function setLatLong (position) {
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
 }
 
 
-  
+
+
+//initialize google map
+function initialize() {
+  //properties for the gmap
+  var mapProp = {
+    center:new google.maps.LatLng(lat, long), //use the latlng set by get location
+    zoom:12,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  //set the map object to map variable
+  map = new google.maps.Map(document.getElementById("gmaps-canvas"), mapProp);
+}
+
+function recenterMap() {
+  map.setCenter(new google.maps.LatLng(lat, long));
+}
+
+$(document).ready(function(){
+  getLocation();
+  initialize();
+})
+
 //trigger redraw on modal click
 $(document).on('click', '#location-button', function(){
   
-  initialize();
+  
+  //gmaps initialization
+  
    
+  //redraw map once modal is open
   google.maps.event.addListenerOnce(map, 'idle', function() {
     google.maps.event.trigger(map, 'resize');
+    
   });
+
+  //recenter map on lat, long variable
+  setTimeout(recenterMap, 2000);
 
 })
 
