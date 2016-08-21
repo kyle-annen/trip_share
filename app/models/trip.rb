b1303371395d74a539229b8cbc50f08f224e11c5
Kyle Annen <kyleannen@gmail.com>
2016-08-21 15:32:22 -0700

    disable paperclip validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,10 +10,11 @@ class Trip < ActiveRecord::Base
     styles: {large: "600x400>", medium: "300x200>", thumb: "100x75>" }, 
     default_url: "public/images/:style/missing.png"
   crop_attached_file :image, :aspect => "3:2"
-  validates_attachment :image, 
-    content_type: {content_type: ["image/jpeg", "image/png"]}
-  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
-  
+  ###
+  # validates_attachment :image, 
+  #   content_type: {content_type: ["image/jpeg", "image/png"]}
+  # validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
+  do_not_validate_attachment_file_type :image
 
   #validations
   validates :name, presence: true
