49f1f5f1f05662e9b35d8e92dbb0e98686bb24cf
Kyle Annen <kyleannen@gmail.com>
2016-08-21 14:42:34 -0700

    adjust trip model paperclip order

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -9,8 +9,9 @@ class Trip < ActiveRecord::Base
   has_attached_file :image, 
     styles: {large: "600x400>", medium: "300x200>", thumb: "100x75>" }, 
     default_url: "public/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   crop_attached_file :image, :aspect => "3:2"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
 
   #validations
   validates :name, presence: true
